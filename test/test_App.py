70e40b159b82f7d073aab753bcfcd997440aa48e
Mr Kosay <kosa11840@gmail.com>
2025-11-10 23:32:42 +0200

    edit

--- test/test_App.py
+++ test/test_App.py
@@ -4,10 +4,8 @@ from app import greet
 
 class TestApp(unittest.TestCase):
 
-
     def test_greet(self):
-        self.assertEqual(greet("World"),"Hello, World from FirstName LastName!")
- 
+        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
 
 
 if __name__ == "__main__":
